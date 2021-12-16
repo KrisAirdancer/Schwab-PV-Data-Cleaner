bee1880dd4025c1ec01dd0405f2f85f13e4716a0
Kris Airdancer <chris.scott.marston@gmail.com>
2021-12-16 14:35:09 -0700

    Working

--- src/Schwab PV Data Cleaner.py
+++ src/Schwab PV Data Cleaner.py
@@ -1,6 +1,24 @@
 # Schwab Personal Value Data Cleaner
 
-filepath = input("Input Filepath: ")
+#rawfilepath = input('Input Raw Filepath: ')
+#cleanfilepath = input('Input Clean Filepath: ')
+#INSERT TESTING CODE FROM OBSIDIAN HERE
 
-file = open(filepath)
+try:
+    rawData = open(rawfilepath, 'r')
+    cleanData = open(cleanfilepath, 'r')
+    #fileAsString = rawData.read()
+    #print('FILE CONTENTS: ', fileAsString)
+
+    for line in rawData :
+        print(line)
+
+
+
+    rawData.close()
+    cleanData.close()
+except:
+    print('Invalid File.')
+
+
 
