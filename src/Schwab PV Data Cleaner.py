d21aa003f6e98a1d1657be3d2d52ee862b9b7b55
Kris Airdancer <chris.scott.marston@gmail.com>
2021-12-16 20:13:51 -0700

    Complete

--- src/Schwab PV Data Cleaner.py
+++ src/Schwab PV Data Cleaner.py
@@ -1,24 +1,52 @@
+import traceback
+import sys
+
 # Schwab Personal Value Data Cleaner
 
-#rawfilepath = input('Input Raw Filepath: ')
-#cleanfilepath = input('Input Clean Filepath: ')
-#INSERT TESTING CODE FROM OBSIDIAN HERE
+rawfilepath = input('Input Raw Filepath: ')
+cleanfilepath = input('Input Clean Filepath: ')
+#INSERT EASY-RUN CODE FROM OBSIDIAN HERE
 
 try:
     rawData = open(rawfilepath, 'r')
-    cleanData = open(cleanfilepath, 'r')
-    #fileAsString = rawData.read()
-    #print('FILE CONTENTS: ', fileAsString)
+    cleanData = open(cleanfilepath, 'w') # Note: file must be empty - contain no data
+    holidaysData = open(holidaysfilepath, 'r')
+
+    holidays = holidaysData.readlines()
 
     for line in rawData :
-        print(line)
+        line = line.rstrip() # Remove newline character from all lines
+        splitLine = line.split()
+        del splitLine[-1]
 
+        dateOnly = ''
+        index = 0
+        while index < len(splitLine) : # Convert splitLine back into string
+            dateOnly = dateOnly + ' ' + splitLine[index]
+            index += 1
+        
+        if line.startswith('Saturday') or line.startswith('Sunday') :
+            continue
 
+        isHoliday = False
+        for date in holidays :
+            date = date.rstrip()
+            dateOnly = dateOnly.rstrip()
+            if date in dateOnly :
+                isHoliday = True
+                continue
+        if not isHoliday :
+            line = line + '\n' # Add newline character back for proper file formatting
+            cleanData.write(line)
 
+    # Close files
     rawData.close()
     cleanData.close()
-except:
+    holidaysData.close()
+except Exception:
+    print(traceback.format_exc())
     print('Invalid File.')
+    
 
 
 
